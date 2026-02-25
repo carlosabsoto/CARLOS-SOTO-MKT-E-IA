ac546298bc1d576f2c4f3769a7561499aca027fc
carlosabsoto <carlosabsoto@gmail.com>
2026-02-25 16:25:00 -0300

    Add paths.js for ESPIRITOS data file generation

---
+++ ser-magnetico-api/domains/espiritos-miasmas/paths.js
@@ -0,0 +1,22 @@
+export default {
+  portais: (n) =>
+    `ESPIRITOS/FECHAMENTO-PORTAIS/FECHAMENTO-PORTAL-${n}.txt`,
+
+  pactos: (n) =>
+    `ESPIRITOS/CANCELAMENTO-PACTOS/CANCELAMENTO-PACTO-${n}.txt`,
+
+  espiritos: (n) =>
+    `ESPIRITOS/LIBERACAO-ESPIRITOS/LIBERACAO-${n}.txt`,
+
+  energias: (n) =>
+    `ESPIRITOS/ENERGIAS-DENSAS/ENERGIA-${n}.txt`,
+
+  associacoes: (n) =>
+    `ESPIRITOS/ASSOCIACOES-EMOCIONAIS/ASSOCIACAO-${n}.txt`,
+
+  miasmas: (n) =>
+    `ESPIRITOS/MIASMAS/MIASMA-${n}.txt`,
+
+  mantras: (n) =>
+    `ESPIRITOS/MANTRAS/MANTRA-${n}.txt`
+}
