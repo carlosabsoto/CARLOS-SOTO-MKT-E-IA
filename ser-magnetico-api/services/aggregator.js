6694f41dae36b08952c54458a98b48da0cb2bd7f
carlosabsoto <carlosabsoto@gmail.com>
2026-02-25 20:11:52 -0300

    Update aggregator.js

--- ser-magnetico-api/services/aggregator.js
+++ ser-magnetico-api/services/aggregator.js
@@ -2,17 +2,24 @@ import { fetchFromGitHub } from "./githubService.js";
 
 export async function aggregateData(dados, paths) {
   const resultado = {};
-
   const promises = [];
 
   for (const categoria in dados) {
+
+    if (!paths[categoria]) {
+      console.warn(`Categoria ignorada: ${categoria}`);
+      continue;
+    }
+
+    if (!Array.isArray(dados[categoria])) continue;
+
     resultado[categoria] = {};
 
-    for (const numero of dados[categoria]) {
-      const path = paths[categoria](numero);
+    for (const item of dados[categoria]) {
+      const path = paths[categoria](item);
 
       const promise = fetchFromGitHub(path).then((conteudo) => {
-        resultado[categoria][numero] = conteudo;
+        resultado[categoria][item] = conteudo;
       });
 
       promises.push(promise);
