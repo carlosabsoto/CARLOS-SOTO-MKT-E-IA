e7b33d5818b53b70b4135dab80fea0b9961e9812
carlosabsoto <carlosabsoto@gmail.com>
2026-02-25 19:35:41 -0300

    Add paths.js for DAM file path generation

---
+++ ser-magnetico-api/domains/dam/paths.js
@@ -0,0 +1,19 @@
+export default {
+  cartas: (n) =>
+    `DAM/CARTAS/CARTA-${n}.txt`,
+
+  areasSistemicas: (n) =>
+    `DAM/AREAS-SISTEMICAS/AREA-SISTEMICA-${n}.txt`,
+
+  areasDeAtuacao: (n) =>
+    `DAM/AREAS-DE-ATUACAO/AREA-DE-ATUACAO-${n}.txt`,
+
+  desativacoes: (n) =>
+    `DAM/DESATIVACOES/DESATIVACAO-${n}.txt`,
+
+  ativacoes: (n) =>
+    `DAM/ATIVACOES/ATIVACAO-${n}.txt`,
+
+  mantras: (tipo) =>
+    `DAM/MANTRAS/${tipo}`
+}
