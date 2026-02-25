55b58768a63528fba554283d24a8890ce20dca72
carlosabsoto <carlosabsoto@gmail.com>
2026-02-25 16:24:06 -0300

    Create config.js

---
+++ ser-magnetico-api/domains/espiritos-miasmas/config.js
@@ -0,0 +1,12 @@
+export default {
+  nome: "espiritos-miasmas",
+  categorias: [
+    "portais",
+    "pactos",
+    "espiritos",
+    "energias",
+    "associacoes",
+    "miasmas",
+    "mantras"
+  ]
+}
