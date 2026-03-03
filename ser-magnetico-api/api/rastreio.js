1de83aef6574239c0e4a3f2ff5251ef4af8d35ec
carlosabsoto <carlosabsoto@gmail.com>
2026-03-03 12:59:19 -0300

    Update rastreio.js

--- ser-magnetico-api/api/rastreio.js
+++ ser-magnetico-api/api/rastreio.js
@@ -1,7 +1,7 @@
 import espiritosPaths, { resolvePath as resolveEspiritos } from "../domains/espiritos-miasmas/paths.js";
 import damPaths, { resolvePath as resolveDam } from "../domains/dam/paths.js";
 import bioHumanoPaths, { resolvePath as resolveBioHumano } from "../domains/bio-humano/paths.js";
-import bioAnimalPaths, { resolvePath as resolveBioAnimal } from "../domains/bio-animal/paths.js";
+//import bioAnimalPaths, { resolvePath as resolveBioAnimal } from "../domains/bio-animal/paths.js";
 
 import { aggregateData } from "../services/aggregator.js";
 import { validateRequest } from "../services/validator.js";
