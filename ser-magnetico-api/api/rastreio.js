d0da8c844cbf911fed21b60c2918e1c7a361fef6
carlosabsoto <carlosabsoto@gmail.com>
2026-03-05 19:50:28 -0300

    Update rastreio.js

--- ser-magnetico-api/api/rastreio.js
+++ ser-magnetico-api/api/rastreio.js
@@ -238,13 +238,4 @@ export default async function handler(req, res) {
     });
 
   }
-
-const resposta = aggregateData(...);
-
-const tamanho = JSON.stringify(resposta).length;
-
-console.log("TAMANHO RESPOSTA:", tamanho);
-
-return res.status(200).json(resposta);
-
 }
