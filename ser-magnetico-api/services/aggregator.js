f44c405fd9ab187016c5a78cd5575a58cfd2f2b5
carlosabsoto <carlosabsoto@gmail.com>
2026-03-05 14:16:26 -0300

    Update aggregator.js

--- ser-magnetico-api/services/aggregator.js
+++ ser-magnetico-api/services/aggregator.js
@@ -45,13 +45,16 @@ export async function aggregateData(dados, paths, resolvePath) {
       try {
 
         const path = resolvePath(categoria, numero, paths);
+        
         console.log("PATH RESOLVIDO:", categoria, numero, path);
 
         if (!path) {
           console.warn("Path não encontrado:", categoria, numero);
           continue;
         }
-
+        
+        console.log("Baixando arquivo:", path);
+        
         const task = fetchCached(path).then((conteudo) => ({
           categoria,
           numero,
