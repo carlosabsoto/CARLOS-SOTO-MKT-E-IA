420e63c1f41e8ba9b63e3d7ca2323c116757bb2a
carlosabsoto <carlosabsoto@gmail.com>
2026-03-10 20:40:00 -0300

    Update rastreio.js

--- ser-magnetico-api/api/rastreio.js
+++ ser-magnetico-api/api/rastreio.js
@@ -150,6 +150,7 @@ export default async function handler(req, res) {
     if (cursoRaw === "dam" && body.texto) {
 
       console.log("🔎 PARSING TEXTO DAM");
+      console.log("📝 TEXTO RECEBIDO:", body.texto); 
 
       dados = parseRastreioDAM(body.texto);
 
